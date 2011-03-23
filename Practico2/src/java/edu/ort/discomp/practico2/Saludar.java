5b555222092c8d4f430b3553e14ad52439e8e5b4
migueldiab <migueldiab@c19039e0-b999-482a-a7c2-be7dbe6476a1>
2011-03-23 01:16:38 +0000

    Con polling
    git-svn-id: https://xp-dev.com/svn/DisComp/trunk@20 c19039e0-b999-482a-a7c2-be7dbe6476a1

--- Practico2/src/java/edu/ort/discomp/practico2/Saludar.java
+++ Practico2/src/java/edu/ort/discomp/practico2/Saludar.java
@@ -2,6 +2,8 @@ package edu.ort.discomp.practico2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +29,15 @@ public class Saludar extends HttpServlet {
     writer.println("<h1>");
     writer.println("Hola ");
     writer.println(parameter);
+
+    writer.println("Soy un proceso de long polling...<br/>");
+    for (int i= 0; i < 100; i++) {
+      writer.println("Me llamaron " + i + " veces<br/>");
+      writer.flush();
+      for (int j= 0; j < 10000; j++) {
+        System.out.println("Test///");
+      }
+    }
     writer.println("</h1><br/>");
     
     
